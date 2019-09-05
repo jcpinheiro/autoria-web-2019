f94e5bae784f8bf8b6b8d579b5b4c80a7afac42c
jcpinheiro <jcpinheiro@ifma.edu.br>
2019-09-05 08:25:33 -0300

    Finalização do código javascript de pedido de vendas

---
+++ 05-javascript-pedido-venda-oo-ecmascript-5/js/Pedido.js
@@ -0,0 +1,41 @@
+function constroiPedido() {
+
+    let pedido = {
+
+        itens: [],
+
+
+        adiciona: function (item ) {
+            this.itens.push(item );
+        },
+
+        adicionaItens: function(...itens ) {
+            this.itens.push(itens );
+        },
+
+        getTotal: function () {
+            let total = 0;
+          
+ /*        for(let i=0; i < this.itens.length; i++) {
+                total = total + itens[i].getSubTotal();
+            }
+ */
+            this.itens.forEach(function (item) {
+                total = total + item.getSubTotal();
+            });
+            return total;
+        },
+
+        remove: function(item ) {
+          
+            for(let i = 0; i < this.itens.length; i++) {
+
+               if ( this.itens[i].comparaItem(item ) ) {
+                  console.log('Remove ' + itens[i]);
+               }
+            }
+        }
+    };
+    return pedido;
+}
+
